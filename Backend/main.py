7c0540b4efc04daf580cd0413c9188c2662590fe
0scar-M <21omccartney@wakatipu.school.nz>
2024-05-24 11:36:27 +1200

    Create Backend single-file convert endpoint #3

--- Backend/main.py
+++ Backend/main.py
@@ -1,18 +1,22 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, Query, HTTPException
 from fastapi.responses import Response
+import PIL as pil
 from PIL import Image
 import uuid
 import sqlite3 as sql
 import os
+import io
 
 app = FastAPI()
 
 DATABASE = os.path.join(os.path.dirname(os.path.dirname(__file__)), r"Database/files.db")
-valid_conversions = {"JPG": "PNG", 
-                     "PNG": "JPG"}
+media_formats = {"image": ("JPEG", "PNG")} # tells the API what conversion method to use for which formats
+valid_formats = []
+[[valid_formats.append(y) for y in x] for x in media_formats.values()] # set valid_formats to list of all valid formats
+invalid_conversions = []
 
-@app.post("/upload/{session_id}")
-async def upload_file(session_id: str, file: UploadFile = File(...)):
+@app.post("/upload/")
+async def upload_file(session_id: str = Query(...), file: UploadFile = File(...)):
     """
     Adds entry to files table. 
     If session_id = new, then a new uuid will be returned and a new entry will be created in the db for the file.
@@ -25,19 +29,20 @@ async def upload_file(session_id: str, file: UploadFile = File(...)):
 
     file_name = file.filename
     file_format = file.filename.split(".")[-1].upper()
+    if file_format == "JPG": file_format = "JPEG" # PIL uses JPEG not JPG
     file_bytes = await file.read()
     file_id = file.filename+"|"+session_id
-    session_ids = [x[0] for x in db_query("SELECT session_id FROM files")] # list of tuples of strings -> list of strings
+    session_ids = [x[0] for x in db_query("SELECT session_id FROM files", "getting list of session_ids from database")] # list of tuples of strings -> list of strings
     
-    if file_format not in valid_conversions.keys():
+    if file_format in [x[0] for x in invalid_conversions]:
         raise HTTPException(status_code=400, detail=f"Invalid file format '{file_format}'")
     
     if new_session_id:
-        db_query("INSERT INTO files VALUES (?, ?, ?, ?, ?);", file_name, file_format, file_bytes, session_id, file_id)
+        db_query("INSERT INTO files VALUES (?, ?, ?, ?, ?);", "uploading file to database", file_name, file_format, file_bytes, session_id, file_id)
     elif session_id in session_ids:
-        db_query("UPDATE files SET name=?, format=?, bytes=?, file_id=? WHERE session_id=?;", file_name, file_format, file_bytes, file_id, session_id)
+        db_query("UPDATE files SET name=?, format=?, bytes=?, file_id=? WHERE session_id=?;", "updating file in database", file_name, file_format, file_bytes, file_id, session_id)
     else:
-        raise HTTPException(status_code=404, detail="Item not found")
+        raise HTTPException(status_code=404, detail=f"Invalid session_id value '{session_id}'")
     
     return {
         "file_name": file_name,
@@ -45,13 +50,67 @@ async def upload_file(session_id: str, file: UploadFile = File(...)):
         "session_id": session_id
             }
 
-def db_query(query_body: str, *query_params: tuple):
+@app.get("/convert/")
+async def convert_file(session_id: str = Query(...), to_format: str = Query(...), optimise: bool = Query(...)):
+    """
+    Returns file in desired format.
+    optimise sets the optimize property in Image.save() to True. It is lossless compression only.
+    Will return 404 if the session_id is invalid.
+    """
+    if to_format == "JPG": to_format = "JPEG" # PIL uses 'JPEG' instead of 'JPG'
+
+    file_reponse = db_query("SELECT bytes, format, file_id FROM files WHERE session_id=?", "getting file from database", session_id)
+    if file_reponse:
+        file_reponse = file_reponse[0]
+    else:
+        raise HTTPException(status_code=404, detail=f"File not found for session_id '{session_id}'")
+
+    file_id = file_reponse[2]
+    conversion = (file_reponse[1], to_format)
+    match conversion:
+        case (f, t) if f in media_formats["image"] and t in media_formats["image"] and (f, t) not in invalid_conversions:
+            # Image conversion
+            try:
+                image = Image.open(io.BytesIO(file_reponse[0]))
+                converted_io = io.BytesIO()
+                image.save(converted_io, format=conversion[1], optimize=optimise) # optimize=True enables compression (lossless)
+            except pil.UnidentifiedImageError as e:
+                # corrupted file
+                db_query("DELETE FROM files WHERE file_id=?", "removing file from database", file_id)
+                raise_error(e, "converting image", code=400)
+            except Exception as e:
+                # remove the file from the db because if there was a conversion error, they will have to re-upload a file anyway.
+                db_query("DELETE FROM files WHERE file_id=?", "removing file from database", file_id)
+                raise_error(e, "converting image")
+        case _:
+            raise HTTPException(status_code=400, detail=f"Invalid file conversion '{' to '.join(conversion)}'")
+    
+    db_query("DELETE FROM files WHERE session_id=?", "removing file from database", session_id)
+    return Response(content=converted_io.getvalue())
+
+def db_query(query_body: str, error_action: str, *query_params: tuple):
     """
     Executes a query on the database, returns the response and commits the changes.
     query_body = query to be executed.
     *query_params = parameterized parameters for query.
+    action = what this query does, used for descriptive error msgs.
     """
-    with sql.connect(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute(query_body, query_params)
-        return cursor.fetchall()
+    try:
+        with sql.connect(DATABASE) as conn:
+            cursor = conn.cursor()
+            cursor.execute(query_body, query_params)
+            return cursor.fetchall()
+    except Exception as e:
+        raise_error(e, error_action)
+
+def raise_error(error, error_action, code=500):
+    """
+    Raises an HTTPException if result is Exception.
+    async so that if there is an error, it waits before continuing.
+    """
+    print(type(error), error_action)
+    if issubclass(type(error), Exception):
+        if error_action:
+            raise HTTPException(status_code=code, detail=f"Error while {error_action}: {error}")
+        else:
+            raise HTTPException(status_code=code, detail=f"Error: {error}")
